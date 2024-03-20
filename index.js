0829cad121f6f4ea0e0fa66ca4664321cbfe06bc
WolfpackHerlev <162029131+WolfpackHerlev@users.noreply.github.com>
2024-03-20 15:16:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 // Array de desportos (exemplo)
 var desportos = [
-    { nome: "Futebol", jogos: [
-        { campeonato: "Campeonato A", equipe1: "Equipe A", equipe2: "Equipe B", palpite: "Vitória Time A", odds: 2.00, resultado: "-" },
-        { campeonato: "Campeonato B", equipe1: "Equipe C", equipe2: "Equipe D", palpite: "Empate", odds: 3.50, resultado: "-" }
-        // Adicione mais jogos conforme necessário para o desporto de futebol
+    { nome: "Football Today", jogos: [
+        { campeonato: "EQ1", equipe1: "Deportivo Cuenca", equipe2: "Macara", palpite: "Btts", odds: 1.83, resultado: "-" },
+        { campeonato: "ENG NL", equipe1: "Wealdstone", equipe2: "Bromley FC", palpite: "Under 2.5", odds: 2.07, resultado: "-" },
+          { campeonato: "ENG NL", equipe1: "Halifax", equipe2: "Chesterfield", palpite: "Away", odds: 2.00, resultado: "-" },
+        { campeonato: "ENG NLs", equipe1: "Taunton", equipe2: "Chippenham Town", palpite: "Away to score", odds: "-", resultado: "-" },
+          { campeonato: "MX2", equipe1: "Tapatio", equipe2: "Correcaminos", palpite: "Under 2.5", odds:1.75, resultado: "-" }
+        
     ] },
-    { nome: "Basquete", jogos: [
-        // Jogos para o desporto de basquete
+    { nome: "Basketball Today", jogos: [
+        { campeonato: "BR-1", equipe1: "Sao Jose", equipe2: "Cerrado", palpite: "1ST Period(Over 40.5)", odds: 1.80, resultado: "-" },
+        { campeonato: " BR-1", equipe1: "Sao Paulo", equipe2: "Caxias Do Sul", palpite: "Home", odds:"-", resultado: "-" },
+          { campeonato: "BR-1", equipe1: "Mogi", equipe2: "Brasilia", palpite: "Away Over 75.5", odds: "-", resultado: "-" },
+        { campeonato: "NBA", equipe1: "Detroit Pistons", equipe2: "Indiana Pasters", palpite: "Away", odds: "-", resultado: "-" },
+          { campeonato: " NBA", equipe1: "Cleveland Cavaliers", equipe2: "Miami Heat", palpite: "Over 216.5", odds: "-", resultado: "-" },
     ] },
-    { nome:"Ice hockey",jogos: [
-        {campeonato:"Asobal",equipe1:"Armada",equipe2:"Albacete",palpite:"Albacete",odds:1.45,resultado:"-"}
-        ]},
-    { nome:"Handball",jogos:[
-        {campeonato:"ESO",equipe1:"Ulm",equipe2:"Biel",palpite: "Over",odds:1.53,resultado:"-"}
-        ]},
-{ nome:"Handball",jogos:[
-        {campeonato:"ESO",equipe1:"Ulm",equipe2:"Biel",palpite: "Over",odds:1.53,resultado:"-"}
-        ]},
-{ nome:"Handball",jogos:[
-        {campeonato:"ESO",equipe1:"Ulm",equipe2:"Biel",palpite: "Over",odds:1.53,resultado:"-"}
+    { nome:"Ice hockey Today",jogos: [
+         { campeonato: "CA-OHL", equipe1: "North Bay Battalion", equipe2: "Barrie Colts", palpite: "Away over 2.5", odds: 1.60, resultado: "-" },
+        {campeonato:"CA-OHL",equipe1:"Flint Firebirds",equipe2:"Erie Otters",palpite:"2ND Period(BTS)", odds:"-",resultado:"-"},
+        {campeonato:"CA-QMJ",equipe1:"Victoriaville Tigers",equipe2:"Shawinigan Cataractes",palpite:"1ST Period(BTS)",odds:"-",resultado:"-"},
+        {campeonato:"CA-QMJ",equipe1:"Saint John Sea Dogs",equipe2:"Moncton WildCats",palpite:"1ST Period(HTS)",odds:"-",resultado:"-"},
+        {campeonato:"GB1",equipe1:"Dundee",equipe2:"Guildford",palpite:"Over 5.5",odds:1.58,resultado:"-"},
+        {campeonato:"GB1",equipe1:"Cardiff",equipe2:"Sheffield",palpite:"2ND Period(ATS)",odds:"-",resultado:"-"},
+        {campeonato:"CH1",equipe1:"Zurich",equipe2:"Biel",palpite:"1ST period(ATS)",odds:"-",resultado:""}
         ]}
     // Adicione mais desportos conforme necessário
 ];
