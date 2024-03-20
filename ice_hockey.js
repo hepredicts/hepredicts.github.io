45cc2473890003b11f78f9200a3caf03a02e92f0
WolfpackHerlev <162029131+WolfpackHerlev@users.noreply.github.com>
2024-03-20 15:33:20 +0100

    Update ice_hockey.js

--- ice_hockey.js
+++ ice_hockey.js
@@ -1,16 +1,16 @@
 // Array de desportos (exemplo)
 var desportos = [
-    { nome: "Futebol", jogos: [
-        { campeonato: "Campeonato A", equipe1: "Equipe A", equipe2: "Equipe B", palpite: "Vitória Time A", odds: 2.00, resultado: "-" },
-        { campeonato: "Campeonato B", equipe1: "Equipe C", equipe2: "Equipe D", palpite: "Empate", odds: 3.50, resultado: "-" },
-          { campeonato: "Campeonato A", equipe1: "Equipe A", equipe2: "Equipe B", palpite: "Vitória Time A", odds: 2.00, resultado: "-" },
-        { campeonato: "Campeonato B", equipe1: "Equipe C", equipe2: "Equipe D", palpite: "Empate", odds: 3.50, resultado: "-" },
-          { campeonato: "Campeonato A", equipe1: "Equipe A", equipe2: "Equipe B", palpite: "Vitória Time A", odds: 2.00, resultado: "-" },
-        { campeonato: "Campeonato B", equipe1: "Equipe C", equipe2: "Equipe D", palpite: "Empate", odds: 3.50, resultado: "-" },
-         { campeonato: "Campeonato A", equipe1: "Equipe A", equipe2: "Equipe B", palpite: "Vitória Time A", odds: 2.00, resultado: "-" },
-        { campeonato: "Campeonato B", equipe1: "Equipe C", equipe2: "Equipe D", palpite: "Empate", odds: 3.50, resultado: "-" },
-         { campeonato: "Campeonato A", equipe1: "Equipe A", equipe2: "Equipe B", palpite: "Vitória Time A", odds: 2.00, resultado: "-" },
-        { campeonato: "Campeonato B", equipe1: "Equipe C", equipe2: "Equipe D", palpite: "Empate", odds: 3.50, resultado: "-" },
+    { nome: "Ice Hockey Today", jogos: [
+        { campeonato: "CA-OHL", equipe1: "North Bay Battalion", equipe2: "Barrie Colts", palpite: "Away over 2.5", odds: 1.60, resultado: "-" },
+        {campeonato:"CA-OHL",equipe1:"Flint Firebirds",equipe2:"Erie Otters",palpite:"2ND Period(BTS)", odds:"-",resultado:"-"},
+        {campeonato:"CA-QMJ",equipe1:"Victoriaville Tigers",equipe2:"Shawinigan Cataractes",palpite:"1ST Period(BTS)",odds:"-",resultado:"-"},
+        {campeonato:"CA-QMJ",equipe1:"Saint John Sea Dogs",equipe2:"Moncton WildCats",palpite:"1ST Period(HTS)",odds:"-",resultado:"-"},
+        {campeonato:"GB1",equipe1:"Dundee",equipe2:"Guildford",palpite:"Over 5.5",odds:1.58,resultado:"-"},
+        {campeonato:"GB1",equipe1:"Cardiff",equipe2:"Sheffield",palpite:"2ND Period(ATS)",odds:"-",resultado:"-"},
+        {campeonato:"CH1",equipe1:"Zurich",equipe2:"Biel",palpite:"1ST period(ATS)",odds:"-",resultado:""},
+        {campeonato:"CH1",equipe1:"Fribourg",equipe2:"Lugano",palpite:"Away over 1.5",odds:"-",resultado:"-"},
+        {campeonato:"FR2",equipe1:"Chambery",equipe2:"Caen",palpite:"2ND Period(HTS)",odds:"-",resultado:"-"},
+        {campeonato:"DE1",equipe1:"Adler Mannheim",equipe2:"Eisbaren Berlin",palpite:"2ND Period(ATS)",odds:"-",resultado:"-"}
         
     ] }
     // Adicione mais desportos conforme necessário
@@ -26,12 +26,12 @@ function adicionarTabelasDesporto() {
             <caption>${desporto.nome}</caption>
             <thead>
                 <tr>
-                    <th>Campeonato</th>
-                    <th>Equipe 1</th>
-                    <th>Equipe 2</th>
-                    <th>Palpite</th>
+                    <th>League</th>
+                    <th>Home</th>
+                    <th>Away</th>
+                    <th>Prediction</th>
                     <th>Odds</th>
-                    <th>Resultado</th>
+                    <th>Result</th>
                 </tr>
             </thead>
             <tbody id="corpo-tabela-${desporto.nome.replace(/\s/g, '-')}">
