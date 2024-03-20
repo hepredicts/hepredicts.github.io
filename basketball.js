04d13e991fde804d6b831fa239529a3ed17368ed
WolfpackHerlev <162029131+WolfpackHerlev@users.noreply.github.com>
2024-03-20 15:53:27 +0100

    Update basketball.js

--- basketball.js
+++ basketball.js
@@ -23,31 +23,6 @@ function adicionarTabelasDesporto() {
             <caption>${desporto.nome}</caption>
             <thead>
                 <tr>
-                    <th>Campeonato// Array de desportos (exemplo)
-var desportos = [
-    { nome: "Today", jogos: [
-        { campeonato: "BR-1", equipe1: "Sao Jose", equipe2: "Cerrado", palpite: "1ST Period(Over 40.5)", odds: 1.80, resultado: "-" },
-        { campeonato: " BR-1", equipe1: "Sao Paulo", equipe2: "Caxias Do Sul", palpite: "Home", odds:"-", resultado: "-" },
-          { campeonato: "BR-1", equipe1: "Mogi", equipe2: "Brasilia", palpite: "Away Over 75.5", odds: "-", resultado: "-" },
-        { campeonato: "NBA", equipe1: "Detroit Pistons", equipe2: "Indiana Pasters", palpite: "Away", odds: "-", resultado: "-" },
-          { campeonato: " NBA", equipe1: "Cleveland Cavaliers", equipe2: "Miami Heat", palpite: "Over 216.5", odds: "-", resultado: "-" },
-        { campeonato: "CH-1", equipe1: "Nyon", equipe2: "Lugano Tigers", palpite: "Over 172.5", odds: "-", resultado: "-" },
-         { campeonato: "FR-1", equipe1: "Paris", equipe2: "Nancy", palpite: "Under 178.5", odds: "-", resultado: "-" },
-        { campeonato: "CHL", equipe1: "Ludwisburg", equipe2: "Bonn", palpite: "Away Over 75.5", odds: "-", resultado: "-" }
-    ] }
-    // Adicione mais desportos conforme necessário
-];
-
-// Função para adicionar tabelas de desporto
-function adicionarTabelasDesporto() {
-    var containerTabelas = document.getElementById("tabelas");
-
-    desportos.forEach(function(desporto) {
-        var tabela = document.createElement("table");
-        tabela.innerHTML = `
-            <caption>${desporto.nome}</caption>
-            <thead>
-                <tr>
                     <th>League</th>
                     <th>Home</th>
                     <th>Away</th>
@@ -94,7 +69,32 @@ adicionarTabelasDesporto();
 
 
 
-/th>
+ // Array de desportos (exemplo)
+var desportos = [
+    { nome: "Today", jogos: [
+        { campeonato: "BR-1", equipe1: "Sao Jose", equipe2: "Cerrado", palpite: "1ST Period(Over 40.5)", odds: 1.80, resultado: "-" },
+        { campeonato: " BR-1", equipe1: "Sao Paulo", equipe2: "Caxias Do Sul", palpite: "Home", odds:"-", resultado: "-" },
+          { campeonato: "BR-1", equipe1: "Mogi", equipe2: "Brasilia", palpite: "Away Over 75.5", odds: "-", resultado: "-" },
+        { campeonato: "NBA", equipe1: "Detroit Pistons", equipe2: "Indiana Pasters", palpite: "Away", odds: "-", resultado: "-" },
+          { campeonato: " NBA", equipe1: "Cleveland Cavaliers", equipe2: "Miami Heat", palpite: "Over 216.5", odds: "-", resultado: "-" },
+        { campeonato: "CH-1", equipe1: "Nyon", equipe2: "Lugano Tigers", palpite: "Over 172.5", odds: "-", resultado: "-" },
+         { campeonato: "FR-1", equipe1: "Paris", equipe2: "Nancy", palpite: "Under 178.5", odds: "-", resultado: "-" },
+        { campeonato: "CHL", equipe1: "Ludwisburg", equipe2: "Bonn", palpite: "Away Over 75.5", odds: "-", resultado: "-" }
+    ] }
+    // Adicione mais desportos conforme necessário
+];
+
+// Função para adicionar tabelas de desporto
+function adicionarTabelasDesporto() {
+    var containerTabelas = document.getElementById("tabelas");
+
+    desportos.forEach(function(desporto) {
+        var tabela = document.createElement("table");
+        tabela.innerHTML = `
+            <caption>${desporto.nome}</caption>
+            <thead>
+                <tr>
+                    <th>Campeonato</th>
                     <th>Equipe 1</th>
                     <th>Equipe 2</th>
                     <th>Palpite</th>
@@ -140,3 +140,4 @@ adicionarTabelasDesporto();
 
 
 
+ 
